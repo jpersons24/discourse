e5c07093432219d9e00ba275641a99724ce01d6e
Ori Markowitz <ori.markowitz@gmail.com>
2021-04-20 15:56:33 -0400

    fixed frontend token issue

--- discourse-react/src/components/ChatComponent.js
+++ discourse-react/src/components/ChatComponent.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 
-import 'stream-chat-react/dist/css/index.css';
+import "stream-chat-react/dist/css/index.css";
 
+function ChatComponent({ currentUser }) {
+  console.log(currentUser);
 
-
-function ChatComponent({currentUser}){
-
-  console.log(currentUser)
-
-  const chatClient = StreamChat.getInstance('9tbsyvz84awf');
-  const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY3VybHktYmxvY2stOCJ9.tN_s0rO5Lm765N_zuwXJRFzBmUMfksfbEk8LS3ueHkg';
+  const chatClient = StreamChat.getInstance("9tbsyvz84awf");
+  // const userToken =
+  //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY3VybHktYmxvY2stOCJ9.tN_s0rO5Lm765N_zuwXJRFzBmUMfksfbEk8LS3ueHkg";
+  const userToken = currentUser.token;
 
   chatClient.connectUser(
     {
-      id: currentUser.user.id,
+      id: currentUser.user.username,
       name: currentUser.user.username,
-      image: 'https://getstream.io/random_png/?id=curly-block-8&name=curly-block-8',
+      image:
+        "https://getstream.io/random_png/?id=curly-block-8&name=curly-block-8",
     },
-    userToken,
+    userToken
   );
 
-  const channel = chatClient.channel('messaging', 'curly-block-8', {
+  const channel = chatClient.channel("messaging", "curly-block-8", {
     // add as many custom fields as you'd like
-    image: 'https://www.drupal.org/files/project-images/react.png',
-    name: 'Talk about React',
-    members: ['curly-block-8'],
+    image: "https://www.drupal.org/files/project-images/react.png",
+    name: "Talk about React",
+    members: ["curly-block-8"],
   });
 
   return (
-    <Chat client={chatClient} theme='messaging light'>
+    <Chat client={chatClient} theme="messaging light">
       <Channel channel={channel}>
         <Window>
           <ChannelHeader />
@@ -41,6 +49,6 @@ function ChatComponent({currentUser}){
       </Channel>
     </Chat>
   );
-}; 
+}
 
 export default ChatComponent;
