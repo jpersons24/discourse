85bf6f1884aedba228272a6af82c4d7286a1a5f0
Cameron Bisca <cjbisca@gmail.com>
2021-04-20 15:34:10 -0400

    Signup

--- discourse-react/src/App.js
+++ discourse-react/src/App.js
@@ -1,20 +1,27 @@
+import {useState} from 'react'
 import ChatComponent from './components/ChatComponent.js';
 import Login from "./components/Login.js";
+import LandingPage from './components/LandingPage.js'
 import SignUp from "./components/SignUp.js";
 import {Switch,Route} from 'react-router-dom'
 function App(){
+  const [currentUser, setCurrentUser] = useState(null)
+
 
   return (
     <div>
       <Switch>
-        <Route exact path = "/chat">
-          <ChatComponent/>
-        </Route>
         <Route exact path = "/">
+          <LandingPage />
+        </Route>
+        <Route exact path = "/chat">
+          <ChatComponent currentUser={currentUser}/>
+        </Route>
+        <Route exact path = "/login">
           <Login/>
         </Route>
         <Route exact path = "/signup">
-          <SignUp/>
+          <SignUp currentUser={currentUser} setCurrentUser={setCurrentUser}/>
         </Route>
       </Switch>
     </div>
--- discourse-react/src/components/SignUp.js
+++ discourse-react/src/components/SignUp.js
@@ -1,51 +1,79 @@
 import React, {useState} from 'react'
-import { Link } from "react-router-dom";
-function SignUp() {
+import { Link, useHistory } from "react-router-dom";
+
+
+function SignUp({currentUser, setCurrentUser}) {
     const [formData,setFormData] = useState({
         username: "" ,
         password: ""
     })
     const [errors, setErrors] = useState([]);
+    const history = useHistory()
 
     function handleSubmit(e){
         e.preventDefault()
-        return(<div>hello</div>)
+
+        const newUser = {
+            username: formData.username,
+            password: formData.password
+        }
+
+        fetch('http://localhost:3001/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+        })
+        .then(response => response.json())
+        .then(data => {
+            setCurrentUser(data)
+        console.log('Success:', data);
+        history.push('/chat')
+        })
     }
+
+    function handleChange(e){
+        setFormData({...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit} autoComplete="off">
-        <h1>Signup</h1>
+            <h1>Signup</h1>
   
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={(e) => setFormData.username(e.target.value)}
-        />
-  
-  
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          value={formData.password}
-          onChange={(e) => setFormData.password(e.target.value)}
-        />
-  
-        {errors.map((error) => {
-          return <p key={error}>{error}</p>;
-        })}
-           <button type='submit'>
-                Sign Up
-            </button>
-             </form>
+            <label>Username</label>
+            <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+    
+    
+            <label>Password</label>
+            <input
+                type="password"
+                name="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+    
+            {errors.map((error) => {
+            return <p key={error}>{error}</p>;
+            })}
+                <button type='submit'>
+                    Sign Up
+                </button>
+                </form>
 
-            <Link to = "/">
-                Login 
-            </Link>
-       
+                <Link to = "/">
+                    Login 
+                </Link>
+        
   
         </div>
     )
