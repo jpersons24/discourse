28f44e22b4eb5e7e1f3ab82e0d74ae101023d792
Zach <zbayard@gmail.com>
2021-04-20 10:52:55 -0400

    chatstarted

---
+++ discourse-react/src/components/ChatComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = StreamChat.getInstance('9tbsyvz84awf');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY3VybHktYmxvY2stOCJ9.tN_s0rO5Lm765N_zuwXJRFzBmUMfksfbEk8LS3ueHkg';
+
+chatClient.connectUser(
+  {
+    id: 'curly-block-8',
+    name: 'curly-block-8',
+    image: 'https://getstream.io/random_png/?id=curly-block-8&name=curly-block-8',
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'curly-block-8', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Talk about React',
+  members: ['curly-block-8'],
+});
+
+function ChatComponent(){
+
+  return (
+    <Chat client={chatClient} theme='messaging light'>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+}; 
+
+export default ChatComponent;
