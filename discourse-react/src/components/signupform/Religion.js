c7a7fa9bf4840aa46a83e45fafa618e32fa76480
Zach <zbayard@gmail.com>
2021-04-22 19:00:17 -0400

    zach's_css

--- discourse-react/src/components/signupform/Religion.js
+++ discourse-react/src/components/signupform/Religion.js
@@ -1,14 +1,29 @@
 function Religion({handleChange}) {
    return (
-      <div>
-         <label for="religion">Religion:</label>
-                <select onChange={handleChange} name="religion" id="religion">
-                    <option value="" selected disabled hidden>Choose here</option>
-                    <option value="christianity">Christianity</option>
-                    <option value="jewish">Jewish</option>
-                    <option value="buddhism">Buddhism</option>
-                </select>
-      </div>
+       <div class='login-wrapper'>
+            <div class='question-box'>
+                <p class='question' for="religion">Religion:</p>
+                        <select class='un' onChange={handleChange} name="religion" id="religion">
+                            <option value="" selected disabled hidden>Choose here</option>
+                            <option value="christianity">Christianity</option>
+                            <option value="jewish">Jewish</option>
+                            <option value="buddhism">Buddhism</option>
+                        </select>
+                        <div class="pre-next-buttons">
+                    {currentStep > 1 ? 
+                        <>
+                        <button class="submit previous" onClick={handlePrevious}>Previous</button>
+                        <button class="submit next" onClick={handleNext}>Next</button>
+                            
+                        </>     
+                        : 
+                        null
+                    }
+                    {/* {currentStep < 13 && currentStep > 0 ?  <button class="submit next" onClick={handleNext}>Next</button> : null} */}
+                </div>
+            </div>
+
+       </div>
    )
 }
 
